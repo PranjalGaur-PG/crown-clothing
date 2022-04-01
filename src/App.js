e63981f8be41eddfdad0960a32fad89fd50fe376
PranjalGaur-PG <pranjalgaur.20.12@gmail.com>
2022-04-01 15:49:12 +0530

    Update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import {Routes, Route} from 'react-router-dom';
+
+const HatsPage = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  )
+}
+
+const TopicList = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>Topics Page</h1>
+    </div>
+  )
+}
+
+const TopicDetail = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>Details Page</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path='/' element={ <HomePage /> } />
+        <Route path='/hats' element={ <HatsPage /> } />
+        <Route path='/topics' element={ <TopicList /> } />
+        <Route path='/topics/:topicId' element={ <TopicDetail /> } />
+      </Routes>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -5,7 +5,7 @@ const MenuItem = ({ title, imageUrl, size }) => (
     <div className={`${size} menu-item`}>
         <div className="background-image" style={{ backgroundImage : `url(${imageUrl})` }} />
         <div className="content">
-            <h1 className={title}>HATS</h1>
+            <h1 className={title}>{title.toUpperCase()}</h1>
             <span className="subtitle">Shop Now</span>
         </div>
     </div>
