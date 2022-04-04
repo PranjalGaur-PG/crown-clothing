0b8a0e8a60fa0294fc080fa27dc79fc7d99c2887
PranjalGaur-PG <pranjalgaur.20.12@gmail.com>
2022-04-04 19:10:10 +0530

    Added Header component and links to shop, home and contact page

--- src/App.js
+++ src/App.js
@@ -2,42 +2,15 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import {Routes, Route} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
-
-const HatsPage = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-  )
-}
-
-const TopicList = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>Topics Page</h1>
-    </div>
-  )
-}
-
-const TopicDetail = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>Details Page</h1>
-    </div>
-  )
-}
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path='/' element={ <HomePage /> } />
-        <Route path='/hats' element={ <HatsPage /> } />
-        <Route path='/topics' element={ <TopicList /> } />
-        <Route path='/topics/:topicId' element={ <TopicDetail /> } />
+        {/* <Route path='/contact' /> */}
         <Route path='/shop' element={ <ShopPage /> } />
       </Routes>
     </div>
