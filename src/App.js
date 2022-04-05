3005cebdf55a9c3d1bd3d49d38651a6a8490363f
PranjalGaur-PG <pranjalgaur.20.12@gmail.com>
2022-04-05 16:32:28 +0530

    Added signin Component and custom button component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from './pages/homepage/homepage.component';
 import {Routes, Route} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={ <HomePage /> } />
-        {/* <Route path='/contact' /> */}
+        <Route path='/signin' element={ <SignInAndSignUp /> } />
         <Route path='/shop' element={ <ShopPage /> } />
       </Routes>
     </div>
